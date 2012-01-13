d3af62a5a7763ee842801a15af7dc2cc83ac4812
rambhask <rambhask@ubuntu.(none)>
2012-01-13 11:55:01 -0500

    More Recent prof info...508

--- allmitprof.py
+++ allmitprof.py
@@ -1,7 +1,7 @@
 import re
 import urllib2
 all_prof_info =[]
-for course in range(14,15):
+for course in range(1,21):
     f= file('/home/rambhask/course'+str(course)+'prof.html').read()
     f=f.strip("\n")
     prof_info=re.findall("<dt><a href=\"(.*?)\"><strong>Prof\. (.*?)</strong></a>,.*?,.*?,.*?<.*?>(.*?)</a>\s*</dt>\s*<dd>(.*?)<", f)
@@ -10,6 +10,7 @@ for course in range(14,15):
     prof_info_alt3= re.findall("<dt><strong></strong><a href=\"(.*?)\">.*?\. (.*?)</a>,.*?,.*?,.*?<.*?>(.*?)</a></dt>\s*<dd>(.*?)<",f)
     prof_info_alt4= re.findall("<dt><a href=\"(.*?)\"><strong>.*?\. (.*?)</strong></a>,.*?,.*?,.*?<.*?>(.*?)</a>\s*<strong>\(On Leave\)</strong></dt>\s*<dd>(.*?)<",f)
     prof_info_alt5= re.findall("<dt><strong><a href=\"(.*?)\">.*?\.(.*?)</a></strong>.*?,.*?,.*?<.*?>(.*?)</a>\s*<strong>\(On Leave\)</strong></dt>\s*<dd>(.*?)<",f)
+    prof_info_alt6= re.findall("<dt><strong><a href=\"(.*?)\">.*?\. (.*?)</a></strong>,\s*.*?,\s*.*?,\s*.*?\s*<.*?>(.*?)</a></dt>\s*<dd>(.*?)<",f)
     for prof in prof_info:
         all_prof_info.append(prof)
     for prof in prof_info_alt:
@@ -22,6 +23,8 @@ for course in range(14,15):
         all_prof_info.append(prof)
     for prof in prof_info_alt5:
         all_prof_info.append(prof)
+    for prof in prof_info_alt6:
+        all_prof_info.append(prof)
 print len(all_prof_info)
     
     
@@ -31,6 +34,6 @@ print len(all_prof_info)
 #prof_info=re.findall("<dt><a href=\"(.*?)\"><strong>Prof\. (.*?)</strong></a>,.*?,.*?,.*?<.*?>(.*?)</a>\s*</dt>\s*<dd>(.*?)<", f)
 #prof_info_alt= re.findall("<dt><strong><a href=\"(.*?)\">.*?\. (.*?)</a></strong>,(.*?),(.*?),.*?<.*?>(.*?)</a></dt>\s*<dd>(.*?)<",f)
 
-for prof in all_prof_info:
-    print prof[1]
+#for prof in all_prof_info:
+   # print prof[1]
 import pdb; pdb.set_trace()
