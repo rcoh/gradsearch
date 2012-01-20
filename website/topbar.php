64116e3bc806e3c0013b292bc20819a9909398a1
rambhask <rambhask@ubuntu.(none)>
2012-01-20 10:34:27 -0500

    modified topbar.php; should be able to use it on all the pages now

--- website/topbar.php
+++ website/topbar.php
@@ -4,9 +4,6 @@ session_start();
 echo $_SERVER['HTTP_HOST'];
 echo "---";
 echo $_SERVER['REQUEST_URI'];
-if(strstr($_SERVER['REQUEST_URI'], 'topbar')) {
-   echo "true";
-}
 function insert_active($url) {
     if(strstr($_SERVER['REQUEST_URI'], $url)) {
        echo "class=\"active\"";
@@ -15,10 +12,10 @@ function insert_active($url) {
 
 function loggedin(){
 if (isset($_SESSION['email'])) {
-    echo ">Logged in as " .$_SESSION['email'];
+    echo "Logged in as " .$_SESSION['email'];
 }
 else {
-    echo ">Please log in to view your preferences.";
+    echo "Please log in to view your preferences.";
     }
 }
 
@@ -30,13 +27,13 @@ else {
         <div class="container-fluid">
             <a class="brand" href="index.php">IWantToStud.ty</a>
             <ul class="nav">
-                <li insert_active("index.php")>
+                <li <?php insert_active("index.php"); ?>>
                     <a href="index.php">Home</a>
                 </li>
-                <li insert_active("about.php")>
+                <li <?php insert_active("about.php"); ?>>
                     <a href="about.php">About</a>
                 </li>
-                <li insert_active("contact.php")>
+                <li <?php insert_active("contact.php"); ?>>
                     <a href="contact.php">Contact</a>
                 </li>
             </ul>
@@ -49,7 +46,7 @@ else {
                     <a href="signout.php">Logout</a>
                 </li>
             </ul>
-			<p class="pull-right" style="color:gray; padding-right:10px;" loggedin()>
+			<p class="pull-right" style="color:gray; padding-right:10px;"> <?php loggedin(); ?>
                 <!--Logged in as <a href="profile.php">lalpert</a>-->
                 
             </p>
