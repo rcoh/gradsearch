44f1f31754e19046d783b1f4e992af6d3ced761a
Leah Alpert <lalpert@mit.edu>
2012-01-21 07:52:19 -0500

    productive all-nighter! Added stars, modals to profs.html. Also worked on wiki scraping for related interests

--- data/wiki_topics.py
+++ data/wiki_topics.py
@@ -8,6 +8,7 @@ seed_topics = ["robotics", "neurodevelopment"]
 seed_topics = ["robotics"]
 #seed_topics = ["computational biology"]
 #seed_topics = ["neurodevelopment"]
+seed_topics = ["biochemistry"]
 dict = {}
 
  #also... list of ___ topics
@@ -18,10 +19,25 @@ for topic in seed_topics:
     url_outline = "http://en.wikipedia.org/wiki/Outline_of_" + topic_underscored
     try:
         f1 = util.dl_and_prep(url_outline)
-        print "outline exists!"
-        print f1
-        link_div=re.findall("#See_also(.*?)id=\"See_also\"", f1)
-        print link_div
+        all_links=re.findall("title=\"(.*?)\"", f1)
+
+        for index, link in enumerate(all_links):
+            if "page does not exist" in link:
+                all_links[index] = link[:-22]
+        
+        good_links = []
+        bad_links = []
+        for l in all_links:
+            if (l.count(" ")>3) or (len(l)>35) or (":" in l) or ("(" in l) or ("Outline of" in l) or ("Index of" in l) or ("List of" in l):
+                bad_links += [l]
+            else:
+                good_links += [l]
+
+        print "BAD LINKS:\n"
+        print "\n".join(bad_links)
+        print "\nGOOD LINKS:\n"
+        print "\n".join(good_links)
+
     except: 
         #outline_of page does not exist; look for see_also
         url = "http://en.wikipedia.org/wiki/" + topic_underscored.capitalize()
@@ -29,10 +45,10 @@ for topic in seed_topics:
         link_div=re.findall("id=\"See_also\"(.*?)References", f)
     
     print topic
-    links = re.findall("title=\"(.*?)\"",link_div[0])
+    #links = re.findall("title=\"(.*?)\"",link_div[0])
     
-    print links
-    dict[topic] = links
+    #print links
+    #dict[topic] = links
     
 #print dict
 
