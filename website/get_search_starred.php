aa1bf3ea328cd0297da6e616cc671d4d4e28cbd3
Russell Cohen <rcoh@mit.edu>
2012-01-29 19:31:21 -0500

    Fixing bugs in showing whether or not a search has been starred.

--- website/get_search_starred.php
+++ website/get_search_starred.php
@@ -1,6 +1,7 @@
 <?php
-$starred = search_starred_by_user('?' . $_SERVER["QUERY_STRING"], $_SESSION['user_id']);
-
+$pat = '/&{0,1}start=\d{1,2}&limit=\d{1,2}/';
+$query = preg_replace($pat, '', $_SERVER["QUERY_STRING"]);
+$starred = search_starred_by_user('?' . $query, $_SESSION['user_id']);
 ?>
 <img class="gold_star search_star"  <?php if(!$starred) { echo 'style="display:none"'; } else { echo 'style="display:inline"'; } ?>
   src="gold_star.png">
