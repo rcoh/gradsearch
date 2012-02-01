33f0d3971dab620641ee84be3f9ed4d89589725b
Leah Alpert <lalpert@mit.edu>
2012-01-31 19:25:23 -0500

    made some fixes in autocomplete

--- website/search_autocomplete.php
+++ website/search_autocomplete.php
@@ -4,12 +4,12 @@ require('json.php');
 $con = get_con();
 $return_array = array();
 if ($con) {
-  $query2 = "select name as keyword from prof where match(name) against('$_GET[term]*' in boolean mode) limit 3";
-  $query = "select keyword from keywords where keyword like '" . $_GET['term'] . "%' limit 10";
-  $query = "$query2 union $query";
+  $query2 = "select name as keyword from prof where match(name) against('$_GET[term]*' in boolean mode) limit 10";
+  $query = "select keyword from keywords where keyword like '" . $_GET['term'] . "%' limit 7";
+  $query = "$query union $query2";
   $result = query_or_die($query, $con);
   while ($row = mysql_fetch_array($result)) {
-    $row_array['value'] = $row['keyword'];
+    $row_array['value'] = html_entity_decode($row['keyword'], null, "UTF-8");
     array_push($return_array, $row_array);
   }
 }
