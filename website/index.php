99fa03135bc558815d662141c8dc111298ec1309
Leah Alpert <lalpert@mit.edu>
2012-08-27 22:06:57 -0400

    Added google analytics tracking code to homepage

--- website/index.php
+++ website/index.php
@@ -38,6 +38,20 @@ $num_schools = mysql_num_rows($schools);
         <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
         <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
         <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+        <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-34379847-1']);
+        _gaq.push(['_trackPageview']);
+
+          (function() {
+              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+
+      </script>
     </head>
 <script type="text/javascript" charset="utf-8">
   var is_ssl = ("https:" == document.location.protocol);
